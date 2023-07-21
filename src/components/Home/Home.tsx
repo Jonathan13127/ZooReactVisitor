14630824b74b1ea9958a0caef14d812087877a58
Jonathan13127 <jonathanmal@hotmail.fr>
2023-07-21 11:36:42 +0200

    Last

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,69 +1,69 @@
 import axios from "axios"
-import {FormEvent, useState, useEffect} from "react";
+import { FormEvent, useState, useEffect } from "react";
 import FormSpace from "../FormSpace/FormSpace"
 import FormSpaceExit from "../FormSpace/FormSpaceExit";
 
-function Home(){
+function Home() {
 
     const [zooId, setZooId] = useState<string>("");
     const [zooName, setZooName] = useState<string>()
-    const [zooStatus, setZooStatus] = useState<string>()
+    const [zooStatus, setZooStatus] = useState<boolean>()
 
-    const {baseUri} = require("../../baseUri")
+    const { baseUri } = require("../../baseUri")
     const path = "/zoo"
 
     const [message, setMessage] = useState('')
 
-    async function GetZoo(){
+    async function GetZoo() {
         try {
-            const response = await axios.get(encodeURI(baseUri+path+"/649b4b3cb65880740cbc7019"))
+            const response = await axios.get(encodeURI(baseUri + path + "/649b4b3cb65880740cbc7019"))
             setZooId(response.data._id);
             setZooName(response.data.name);
 
-            if(response.data.status){
-                setZooStatus("Ouvert")
-            }else{
-                setZooStatus("Fermé")
+            if (response.data.status) {
+                setZooStatus(true)
+            } else {
+                setZooStatus(false)
             }
-        } catch (err:unknown) {
-            console.log(err)
-        }
-    }
-  
-    async function enterZoo(){
-        try {
-            const headers = {
-                'Content-Type': 'application/json',
-                'zoo': zooId
-            }
-            const response = await axios.get(encodeURI(baseUri+path+"/enter"),{headers:headers})
-            console.log(response.data)
-            setMessage("Un nouveau visiteur...")
-        } catch (err:unknown) {
+        } catch (err: unknown) {
             console.log(err)
         }
     }
 
-    async function exitZoo(){
+    async function enterZoo() {
         try {
             const headers = {
                 'Content-Type': 'application/json',
                 'zoo': zooId
             }
-            const response = await axios.get(encodeURI(baseUri+path+"/exit"),{headers:headers})
+            const response = await axios.get(encodeURI(baseUri + path + "/enter"), { headers: headers })
+            console.log(response.data)
+            setMessage("Un nouveau visiteur...")
+        } catch (err: unknown) {
+            console.log(err)
+        }
+    }
+
+    async function exitZoo() {
+        try {
+            const headers = {
+                'Content-Type': 'application/json',
+                'zoo': zooId
+            }
+            const response = await axios.get(encodeURI(baseUri + path + "/exit"), { headers: headers })
             console.log(response.data)
             setMessage("Un visiteur est pati...")
 
-        } catch (err:unknown) {
+        } catch (err: unknown) {
             console.log(err)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         GetZoo();
-    },[])
+    }, [])
 
-    return(
+    return (
         <main className="flex flex-column">
             <div className="mx-auto mt-12 w-2/3">
                 <div className="">
@@ -73,33 +73,37 @@ function Home(){
                                 <div className="card-body items-center text-center rounded-sm">
                                     <h2 className="card-title font-bold">{zooName}</h2>
                                     <p className="font-semibold">ID: {zooId}</p>
-                                    <p className="font-semibold">Statut: {zooStatus}</p>
+                                    <p className="font-semibold">Statut: {zooStatus ? 'Ouvert' :'Fermé'}</p>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <div className="card w-96 bg-neutral m-3 text-neutral-content">
-                                <div className="card-body items-center text-center rounded-sm space-y-2">
-                                    <h2 className="card-title font-bold">Simulation entrée zoo:</h2>
-                                    <button className="btn btn-accent m-0" onClick={enterZoo}>Entrer</button>
+                        {zooStatus === true &&
+                            <div className="flex flex-row flex-wrap justify-center items-center">
+                                <div>
+                                    <div className="card w-96 bg-neutral m-3 text-neutral-content">
+                                        <div className="card-body items-center text-center rounded-sm space-y-2">
+                                            <h2 className="card-title font-bold">Simulation entrée zoo:</h2>
+                                            <button className="btn btn-accent m-0" onClick={enterZoo}>Entrer</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="card w-96 bg-neutral m-3 text-neutral-content">
+                                        <div className="card-body items-center text-center rounded-sm space-y-2">
+                                            <h2 className="card-title font-bold">Simulation sortie zoo:</h2>
+                                            <button className="btn btn-accent m-0" onClick={exitZoo}>Sortir</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <FormSpace zoo={zooId} session={""} space={""} />
+                                </div>
+                                <div>
+                                    <FormSpaceExit zoo={zooId} session={""} space={""} />
                                 </div>
                             </div>
-                        </div>
-                        <div>
-                            <div className="card w-96 bg-neutral m-3 text-neutral-content">
-                                <div className="card-body items-center text-center rounded-sm space-y-2">
-                                    <h2 className="card-title font-bold">Simulation sortie zoo:</h2>
-                                    <button className="btn btn-accent m-0" onClick={exitZoo}>Sortir</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <FormSpace zoo={zooId} session={""} space={""}/>
-                        </div>
-                        <div>
-                            <FormSpaceExit zoo={zooId} session={""} space={""}/>
-                        </div>
-                    </div> 
+                        }
+                    </div>
                 </div>
             </div>
             {
